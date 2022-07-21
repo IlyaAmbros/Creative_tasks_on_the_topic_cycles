ca9b605f85180ccbad18e217454a8be53eccce63
IlyaAmbros <108877476+IlyaAmbros@users.noreply.github.com>
2022-07-21 09:29:46 +0300

    Homework is done

---
+++ 1_patern.py
@@ -0,0 +1,6 @@
+print ( "\n" )
+for x in range ( 1 , 16 ) :
+    print ( "*" , end= " ")
+    if x == 1 or x == 3 or x == 6 or x == 10 :
+        print()
+print ( "\n" )
---
+++ 2_patern.py
@@ -0,0 +1,8 @@
+print ( "\n" )
+for x in range ( 1 , 50 ) :
+    print ( "*" , end= " " )
+    if x % 3 == 0 :
+        print( "#" , end= " " )
+    if x % 3 == 0 :
+        print( "#" , end= " " )
+print ( "\n" )
